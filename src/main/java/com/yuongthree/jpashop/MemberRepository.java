10e2cfe7b30418f6bf8ab1aac5c3c0fd84cfbdaa
raiders032 <nameks@naver.com>
2020-05-31 23:31:41 +0900

    Memberm, MemberRepository 추가 테스트 코드 작성

---
+++ src/main/java/com/yuongthree/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package com.yuongthree.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Long save(Member member){
+        entityManager.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return entityManager.find(Member.class, id);
+    }
+}
