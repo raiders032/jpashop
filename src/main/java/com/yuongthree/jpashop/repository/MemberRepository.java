484358eddacac8143ec49900ccda4f1b5a6ab5c3
raiders032 <nameks@naver.com>
2020-06-02 14:48:14 +0900

    상품 리포지토리 개발

--- src/main/java/com/yuongthree/jpashop/repository/MemberRepository.java
+++ src/main/java/com/yuongthree/jpashop/repository/MemberRepository.java
@@ -1,18 +1,18 @@
 package com.yuongthree.jpashop.repository;
 
 import com.yuongthree.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member){
         em.persist(member);
@@ -23,11 +23,11 @@ public class MemberRepository {
     }
 
     public List<Member> findAll(){
-        return em.createQuery("select m from Member m").getResultList();
+        return em.createQuery("select m from Member m", Member.class).getResultList();
     }
 
     public List<Member> findByName(String name){
-        return em.createQuery("select m from Member m where m.name=:name")
+        return em.createQuery("select m from Member m where m.name=:name", Member.class)
                 .setParameter("name",name).getResultList();
     }
 }
