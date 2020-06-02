ebb2f05d293ea2146a73017edd783d596ca71dd9
raiders032 <nameks@naver.com>
2020-06-02 14:38:37 +0900

    상품 도메인 개발

--- src/main/java/com/yuongthree/jpashop/domain/item/Item.java
+++ src/main/java/com/yuongthree/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.yuongthree.jpashop.domain.item;
 
 import com.yuongthree.jpashop.domain.Category;
+import com.yuongthree.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,16 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restQuantity = this.stockQuantity - quantity;
+        if(restQuantity<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restQuantity;
+    }
 }
