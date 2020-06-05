f42e848de7153869cb83d55e1f9225950d3aa9ec
raiders032 <nameks@naver.com>
2020-06-05 18:04:02 +0900

    회원 수정 API

--- src/main/java/com/yuongthree/jpashop/api/MemberApiController.java
+++ src/main/java/com/yuongthree/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package com.yuongthree.jpashop.api;
 
 import com.yuongthree.jpashop.domain.Member;
 import com.yuongthree.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +26,24 @@ public class MemberApiController {
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMember(@RequestBody UpdateMemberRequest request, @PathVariable Long id){
+        memberService.update(id,request.getName());
+        Member member = memberService.findOne(id);
+        return new UpdateMemberResponse(member.getId(), member.getName());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
     }
 
     @Data
