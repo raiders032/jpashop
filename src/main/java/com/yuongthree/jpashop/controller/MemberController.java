2113a1d2837b44dc9a38b5b02ee9ad2bbeea7b82
raiders032 <nameks@naver.com>
2020-06-05 09:29:09 +0900

    회원 목록 조회

--- src/main/java/com/yuongthree/jpashop/controller/MemberController.java
+++ src/main/java/com/yuongthree/jpashop/controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,5 +36,12 @@ public class MemberController {
         member.setAddress(new Address(memberForm.getCity(),memberForm.getStreet(),memberForm.getZipcode()));
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String members(Model model){
+        List<Member> members = memberService.findAll();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
